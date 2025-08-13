b02da828942e7ae022e4e03644ebec0bdfdca3ee
Jaguaratica <kitsuneehh@hotmail.com>
2025-08-13 11:40:30 -0300

    realização do exercicio 1

--- fundamentos/exercicios.js
+++ fundamentos/exercicios.js
@@ -1,4 +1,12 @@
-
-
 // Criar um script que calcula o troco de uma compra. 
 // O utilizador informa o valor total e o valor pago, e o programa calcula o troco.
+
+let total = parseFloat(prompt("Valor total: R$"));  
+let pago = parseFloat(prompt("Valor pagamento: R$"));  
+let troco = total - pago;
+
+if (pago >= total) {
+    console.log("Troco: R$" + (troco));
+} else {
+    console.log("Valor pago é insuficiente!");
+}
