858f24e631aef60694a8408303ef5c3ba43a71c2
evacosta <evacosta.if@gmail.com>
2025-09-05 13:49:15 -0300

    explicacao

---
+++ fundamentos/funcao_tradicional.js
@@ -0,0 +1,37 @@
+// As funções resolvem esse problema ao encapsular um bloco de código que pode ser executado a qualquer momento, simplesmente "chamando" a função pelo seu nome. Os principais motivos para usar funções são:
+
+// Reutilização de Código: Evita a repetição de lógica. Escreva uma vez, use quantas vezes precisar.
+
+// Organização e Legibilidade: Divide um problema complexo em partes menores e mais gerenciáveis. Funções com nomes descritivos (como calcularAreaRetangulo()) tornam o código mais fácil de entender.
+
+// Abstração: Oculta a complexidade de uma determinada tarefa. Você pode usar uma função sem precisar saber os detalhes de sua implementação.
+
+// Manutenibilidade: Facilita a correção de bugs e a implementação de novas funcionalidades. Uma alteração na lógica precisa ser feita em um único lugar: dentro da função.
+
+// Escopo: Cria um escopo local para as variáveis, evitando conflitos com outras partes do seu código.
+
+// Quando Usar Funções?
+// A resposta curta é: sempre que você identificar um bloco de código que executa uma tarefa específica e que pode ser reutilizado ou que torna o seu código mais organizado.
+function somar(a, b) {
+  return a + b;
+}
+
+// [declaracao] -> function
+// [nome] -> somar
+// [parametros] -> (a,b)
+// [corpo da funcao] -> {}
+
+
+// funcao é um verbo, porque ela realiza algo, e ela é usada para que o codigo fique mais organizado e seja possivel reaproveita-lo
+
+// some
+// edite
+
+// somar
+// editar
+
+let diferenca = a - b;
+
+
+
+
