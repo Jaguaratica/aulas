239a9c18b388e811272b6bf029b1cc6d961f3170
Jaguaratica <kitsuneehh@hotmail.com>
2025-10-20 13:49:49 -0300

    atualização 20 10 25

--- revisao/manipulacao_arrays.js
+++ revisao/manipulacao_arrays.js
@@ -10,14 +10,14 @@
 // Exemplo 1: Dobrar os números
 
 // O modo "antigo" (usando forEach, que já é melhor que um 'for' manual)
-numeros = [5, 10, 15, 20, 25];
-dobros = []; // 1. Precisa criar um array vazio
+// numeros = [5, 10, 15, 20, 25];
+// dobros = []; // 1. Precisa criar um array vazio
 
-numeros.forEach(function(numDobro){ // 2. Precisa iterar
-    dobros.push(numDobro * 2) // 3. Precisa "empurrar" (push) manualmente
-});
+// numeros.forEach(function(numDobro){ // 2. Precisa iterar
+//     dobros.push(numDobro * 2) // 3. Precisa "empurrar" (push) manualmente
+// });
 
-console.log(dobros); // Saída: [10, 20, 30, 40, 50]
+// console.log(dobros); // Saída: [10, 20, 30, 40, 50]
 
 // Agora, com .map():
 
@@ -26,11 +26,11 @@ console.log(dobros); // Saída: [10, 20, 30, 40, 50]
 // 1. Diga o que fazer com CADA item (a arrow function)
 // 2. O .map cuida de criar o novo array e adicionar os itens
 // function(numero) { return numero * 2; }
-const dobros = numeros.map((numero) => { 
-    return numero * 2
-});
+// const dobros = numeros.map((numero) => { 
+//     return numero * 2
+// });
 
-console.log(dobros); // Saída: [10, 20, 30, 40, 50]
+// console.log(dobros); // Saída: [10, 20, 30, 40, 50]
 
 // O array de objetos original
 const candidatos = [
@@ -38,5 +38,9 @@ const candidatos = [
     { nome: 'Bruno Costa', idade: 35, vaga: 'Backend' },
     { nome: 'Carla Dias', idade: 23, vaga: 'Frontend' }
 ];
+
+const objetos = candidatos.map((candidato) => {return candidato.nome});
+
+console.log(objetos)
 
 // Usando .map() para transformar cada OBJETO em uma STRING (o nome)
