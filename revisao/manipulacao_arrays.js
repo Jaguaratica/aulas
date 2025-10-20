f421e0439466e693b4ec0cf85efeedc48d10d63c
evacosta <evacosta.if@gmail.com>
2025-10-20 14:02:49 -0300

    explicação sober manipulação de arrays

--- revisao/manipulacao_arrays.js
+++ revisao/manipulacao_arrays.js
@@ -41,6 +41,45 @@ const candidatos = [
 
 const objetos = candidatos.map((candidato) => {return candidato.nome});
 
+const devsBackend = candidatos.filter(candidato => candidato.vaga == "Backend")
+
 console.log(objetos)
+console.log('-------------------------------')
+console.log('devsBackend')
+console.log(devsBackend)
 
 // Usando .map() para transformar cada OBJETO em uma STRING (o nome)
+
+// 2. .filter() - O Selecionador
+// O .filter() é usado quando você quer selecionar itens de um array que passam em um teste.
+
+// Ele NÃO modifica o array original.
+
+// Ele SEMPRE retorna um novo array, mas apenas com os itens que passaram no teste (o array final pode ser menor que o original).
+
+// O modo "antigo" (com forEach e if)
+emails = ["usuario1@gmail.com", "contato@hotmail.com", "dev@gmail.com"];
+emailsValidosFunction = []; // 1. Precisa criar um array vazio
+
+emails.forEach(function(email){
+    if (email.includes("@gmail.com")){ // 2. Precisa de um 'if'
+        emailsValidosFunction.push(email); // 3. Precisa "empurrar" manualmente
+    }
+});
+console.log('-------------------------------')
+console.log(emailsValidosFunction); // Saída: ["usuario1@gmail.com", "dev@gmail.com"]
+
+// Agora, com .filter()
+
+// const emails = ["usuario1@gmail.com", "contato@hotmail.com", "dev@gmail.com"];
+
+// A arrow function é o próprio teste:
+// email.includes("@gmail.com") já retorna true ou false
+const emailsValidos = emails.filter((email) => {
+    return email.includes("@gmail.com")
+});
+
+// true, false, true
+
+console.log(emailsValidos); // Saída: ["usuario1@gmail.com", "dev@gmail.com"]
+
